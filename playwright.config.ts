b085a84a2918ba33f2bf5f8f90372dd19cb0cab5
jsbrittain <98161205+jsbrittain@users.noreply.github.com>
2025-10-21 14:39:28 +0100

    Re-enable end-to-end tests; add test data to repository

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,16 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
-  fullyParallel: true,
+  fullyParallel: false, // tests inside a file must run sequentially
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3030',
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   projects: [
